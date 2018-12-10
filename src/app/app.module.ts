725093bcc37502fc8a01e750a355b008f8b6e9c3
JoonasLeminen <joonas.leminen@outlook.com>
2018-12-10 23:33:44 +0200

    Toolbar muokkausta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {
-  MatButtonModule,
   MatSidenavModule,
   MatToolbarModule,
   MatCardModule,
